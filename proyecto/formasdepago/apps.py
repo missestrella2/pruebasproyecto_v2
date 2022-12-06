6d73d5435c0aa27af082f6b7a7cfd537a6493f00
missestrella2 <missestrella2@gmail.com>
2022-12-05 21:54:27 -0300

    conexion de urls principal con urls de las apps. creacion de urls de login

---
+++ proyecto/formasdepago/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FormasdepagoConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'formasdepago'
---
+++ proyecto/login/urls.py
@@ -0,0 +1,18 @@
+from django.conf import settings
+from django.urls import path, re_path, include
+from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+from . import views
+
+from django.urls import path,include
+
+urlpatterns=[
+
+    path('',views.index,name="index"), 
+ 
+    path('paginaenblanco/',views.paginaenblanco,name="paginaenblanco"),
+    path('accounts/',include('django.contrib.auth.urls')),
+    path('accounts/login/',views.login, name='login'),
+    path('adios/', auth_views.LogoutView.as_view(template_name='login/logout2.html'), name='logout'),
+
+]

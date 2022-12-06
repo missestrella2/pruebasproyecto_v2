4bc187d2838c752ea3a6a814bb6b8a6f0856f283
missestrella2 <111753332+missestrella2@users.noreply.github.com>
2022-12-06 08:25:26 -0300

    Update settings.py

--- proyecto/proyecto/settings.py
+++ proyecto/proyecto/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'nuevadb',
         'USER': 'postgres',
-        'PASSWORD': 'Mi$$9191',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '5432'  
     }

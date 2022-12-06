e111711a317d9a753abbb123a2ce50913a30e7ae
missestrella2 <missestrella2@gmail.com>
2022-12-05 21:55:49 -0300

    views de login

--- proyecto/login/views.py
+++ proyecto/login/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from datetime import datetime
 
-# Create your views here.
+from datetime import datetime
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.template import loader
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.views import View
+from django.views.generic import ListView
+
+
+
+def index(request):
+    return redirect('login')
+
+def logout_view(request):
+     logout(request)
+     return redirect('logout')
+
+
+def paginaenblanco(request):
+    context = {"hoy": datetime.now}
+    return render(request, 'login/paginaenblanco.html', {"context": context})

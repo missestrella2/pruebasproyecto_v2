a1304b03ffb289fb9c381197c630979f331c4719
missestrella2 <missestrella2@gmail.com>
2022-12-09 10:55:43 -0300

    correcion en nombre de los campos de formulario de alta venta. se agrego verbose name

--- proyecto/proyecto/settings.py
+++ proyecto/proyecto/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'nuevadb',
         'USER': 'postgres',
-        'PASSWORD': '',
+        'PASSWORD': 'Mi$$9191',
         'HOST': 'localhost',
         'PORT': '5432'  
     }
--- proyecto/ventas/models.py
+++ proyecto/ventas/models.py
@@ -5,8 +5,8 @@ from formasdepago.models import PagoForma
 
   
 class Venta(models.Model):
-    clientes = models.ForeignKey(Cliente, on_delete=models.CASCADE)
-    pagoformas = models.ForeignKey(PagoForma, on_delete=models.CASCADE)
+    clientes = models.ForeignKey(Cliente, on_delete=models.CASCADE,verbose_name='ID del cliente')
+    pagoformas = models.ForeignKey(PagoForma, on_delete=models.CASCADE, verbose_name='ID de la Forma de Pago')
     fecha_de_venta = models.DateField(verbose_name='Fecha de Venta')
     monto = models.IntegerField(default=0,verbose_name='Monto')
 

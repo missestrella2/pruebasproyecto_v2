957cd93bcc6f8d1b5b9720b44ef3f80e931edc6e
missestrella2 <missestrella2@gmail.com>
2022-12-15 13:30:06 -0300

    se corrigio alta de formulario para que se pueda seleccionar en lista desplegable. se corrigio que pueda aparecer seleccion multiple en departamentos que es many to many.

--- proyecto/ventas/models.py
+++ proyecto/ventas/models.py
@@ -1,10 +1,24 @@
 from django.db import models
-
 from clientes.models import Cliente
 from formasdepago.models import PagoForma
+#from django_utils.choices import Choice, Choices
+#from djchoices import DjangoChoices, ChoiceItem
 
 class Departamento(models.Model):
-    nombre=models.CharField(max_length=100, verbose_name='Nombre del Dpto')
+    nombre=models.CharField(max_length=100, verbose_name='Nombre del Dpto',default='')
+    
+    # no funcionó
+    # nombre=models.CharField(max_length=50,choices=DPTOS_CHOICES,default='1'),
+    
+    #CON DJANGOCHOICES Y DJANGOUTILS2(no funcionó)
+            # class Departamento(DjangoChoices):
+            #     class DTO(Choices):
+            #         seccionferreteria = ChoiceItem("1")
+            #         seccionbazar = ChoiceItem("2")
+            #         seccioncalzado = ChoiceItem("3")
+
+            # # Fields
+            #     nombre = models.CharField(max_length=1, choices=DTO.choices,default=DTO.seccionferreteria)
 
   
 class Venta(models.Model):

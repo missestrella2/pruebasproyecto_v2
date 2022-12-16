06f415a558dfdd89eee1cd031f3c9104f353bad1
missestrella2 <missestrella2@gmail.com>
2022-12-16 12:59:32 -0300

    views de la api

--- proyecto/proyecto_api/views.py
+++ proyecto/proyecto_api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from rest_framework import viewsets
+from rest_framework import permissions
+from clientes.models import Cliente
+from proyecto_api import serializers
+
+class ClienteViewSet(viewsets.ModelViewSet):
+    queryset = Cliente.objects.all()
+    serializer_class = serializers.ClienteSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]

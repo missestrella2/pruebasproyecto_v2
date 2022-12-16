3f2d7b1e857c7c63472abd7ebe71a99179109ecb
missestrella2 <missestrella2@gmail.com>
2022-12-16 13:42:13 -0300

    se movio las views y urls de la app login a la app principal. Se corrigio en views que index iba a la pagina de logiun este o no autenticado. ahora si esta autenticado ya no manda a la pagina de login

--- proyecto/proyecto/urls.py
+++ proyecto/proyecto/urls.py
@@ -1,23 +1,8 @@
-"""proyecto URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls.static import static
 from django.urls import include 
-from django.conf import settings
+from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,7 +11,13 @@ urlpatterns = [
     path('', include('formasdepago.urls')),
     path('', include('clientes.urls')),
 
+    path('',views.index,name='index'), 
+    
     path('accounts/',include('django.contrib.auth.urls')),
+    path('paginaenblanco/',views.paginaenblanco,name="paginaenblanco"),
+    path('accounts/login/',views.login, name='login'),
+    path('adios/',auth_views.LogoutView.as_view(template_name='login/logout.html'), name='logout'),
+    
 
     path('api-auth/',include('rest_framework.urls')), 
     
---
+++ proyecto/proyecto/views.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+
+def index(request):
+    if request.user.is_authenticated:
+        return redirect('paginaenblanco')
+    else:
+        return redirect('login')
+
+def logout_view(request):
+     logout(request)
+     return redirect('logout')
+
+def paginaenblanco(request):
+    context = {"hoy": datetime.now}
+    return render(request, 'login/paginaenblanco.html', {"context": context})

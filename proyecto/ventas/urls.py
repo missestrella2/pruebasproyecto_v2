d0f8439ed538c6f8defb48f2408485baba8d7ff6
missestrella2 <missestrella2@gmail.com>
2022-12-15 23:45:52 -0300

    se crearon las templates y las urls de DEPARTAMENTOS

--- proyecto/ventas/urls.py
+++ proyecto/ventas/urls.py
@@ -4,8 +4,6 @@ from django.contrib.auth.decorators import login_required
 from . import views
 
 urlpatterns=[
-
-    path('ventas/listadeventas/', login_required(views.ListaDeVentas.as_view()), name='listadeventas'),
     path('ventas/altaventaform', login_required(views.altaventaform.as_view()), name='altaventaform'),
    # path('ventas/bajaventaform', login_required(views.bajaventaform.as_view()), name="bajaventaform"),
    # path('ventas/ventaeditar/<int:id_venta>', login_required(views.ventaeditar), name='ventaeditar'),
@@ -14,4 +12,10 @@ urlpatterns=[
 
     path('ventas/buscarventas/', login_required(views.buscarventas), name='buscarventas'),
     path('ventas/estadocambiar/<int:id>', login_required(views.estadocambiar), name='estadocambiar'),
+
+    path('ventas/listadedepartamentos/', login_required(views.ListaDeDepartamentos.as_view()), name='listadedepartamentos'),
+    path('ventas/bajadepartamentoform', login_required(views.bajadepartamentoform.as_view()), name="bajadedepartamentoform"),
+    path('ventas/departamentoeditar/<int:id_venta>', login_required(views.departamentoeditar), name='ventaeditar'),
+    path('ventas/departamentoeliminar/<int:id_venta>', login_required(views.departamentoeliminar), name='ventaeliminar'),
+
 ]
